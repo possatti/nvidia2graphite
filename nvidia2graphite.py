944372155b929c3b20b5199c25fb6ab48b6c281c
Lucas Possatti <lucas_possatti@hotmail.com>
2019-04-25 19:26:49 -0300

    Fix power_readings error and add some fault tolerance

--- nvidia2graphite.py
+++ nvidia2graphite.py
@@ -23,15 +23,30 @@ import subprocess
 import configparser
 from xml.etree import ElementTree
 import re
+import sys
 import time
 import graphitesend
+import argparse
 
 # pylint: disable=invalid-name
 
-if __name__ == '__main__':
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('-c', '--config', default='/etc/nvidia2graphite.conf')
+    parser.add_argument('-n', '--dryrun', action='store_true')
+    parser.add_argument('-d', '--debug', action='store_true')
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = parse_args()
+
     # Read in the config file
     conf = configparser.ConfigParser()
-    conf.read('/etc/nvidia2graphite.conf')
+    conf.read(args.config)
+    if args.debug:
+        print('DEBUG: conf={}'.format(conf), file=sys.stderr)
 
     # Extract metrics
     metrics = []
@@ -39,11 +54,11 @@ if __name__ == '__main__':
         metrics.append(conf['Metrics'][key])
 
     # Extract graphite server information
-    dryrun = False
     graphite_server = conf['Graphite']['host']
     graphite_port = int(conf['Graphite']['port'])
     wait_time = float(conf['Graphite']['interval'])
 
+    g = None
     while 1:
         data = subprocess.check_output(['nvidia-smi', '-q', '-x'])
         root = ElementTree.fromstring(data)
@@ -61,17 +76,31 @@ if __name__ == '__main__':
                 curr_level = gpu
                 for level in metric.split('.'):
                     curr_level = curr_level.find(level)
-                data = re.sub(r"\D", "", curr_level.text)
+                data = re.search(r"(\d+\.?\d*)", curr_level.text).group(1)
                 if data is not None and data != '':
                     metric_dict[metric] = data
 
-            # setup graphite and send dictionary
-            g = graphitesend.init(group='gpu' + str(gpu_id),
-                                  graphite_server=graphite_server,
-                                  graphite_port=graphite_port,
-                                  dryrun=dryrun)
-            g.send_dict(metric_dict)
+            # setup graphite
+            if g is None:
+                try:
+                    g = graphitesend.init(prefix=conf['Graphite']['prefix'],
+                                          group='gpu' + str(gpu_id),
+                                          graphite_server=graphite_server,
+                                          graphite_port=graphite_port,
+                                          dryrun=args.dryrun)
+                except graphitesend.graphitesend.GraphiteSendException as e:
+                    print(repr(e), file=sys.stderr)
+                    print('Will retry again in {} seconds.'.format(wait_time), file=sys.stderr)
+
+            # send dictionary
+            if g is not None:
+                sent = g.send_dict(metric_dict)
+                if args.debug:
+                    print('DEBUG: sent={}'.format(sent), file=sys.stderr)
 
             gpu_id += 1
 
         time.sleep(wait_time)
+
+if __name__ == '__main__':
+    main()
